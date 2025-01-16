6903d5f3beae9b3ab82fbaa53255e32f38d451c1
arias-ds3 <ariasfa7@gmail.com>
2025-01-16 13:25:23 -0300

    message questions component

--- src/Components/QuestionComponent.js
+++ src/Components/QuestionComponent.js
@@ -3,22 +3,30 @@ import { useState } from "react";
 let QuestionsComponent = (props) => {
     let { questions, setPoints, points } = props;
     let [numberOfQuestion, setNumberOfQuestion] = useState(0)
-
+    let [message, setMessage] = useState(null)
 
     let anwserQuestion = (anwser) => {
         if (anwser == questions[numberOfQuestion].anwser){
              // points
+             setMessage("Right anwser")
              setPoints(points+1)
              // mdg
         } else {
+            setMessage("Wrong anwser")
             // msg
         }
 
         if (numberOfQuestion < questions.length-1 ){
             setNumberOfQuestion(numberOfQuestion+1)
+            setTimeout( () => {
+                setMessage(null)
+            }, 2000)
         } else {
             // msg
-            setTimeout(() => {
+            setTimeout( () => {
+                setMessage("Finish")
+            } ,2000)
+            setTimeout( () => {
                 setPoints(0)
                 setNumberOfQuestion(0)
             } ,4000)
@@ -28,13 +36,17 @@ let QuestionsComponent = (props) => {
 
     return (
         <div className="questions"> 
+            { message != null && 
+                <h2> { message } </h2>
+            }
             <div>
                 <h2>Question</h2>
                 <p id="questionsText"> { questions[numberOfQuestion].text } </p>
                 <ul className="options">
-                    <li><button onClick={ () => { anwserQuestion(0) }}> {questions[numberOfQuestion].options[0]} </button></li>
-                    <li><button onClick={ () => { anwserQuestion(1) }}> {questions[numberOfQuestion].options[1]} </button></li>
-                    <li><button onClick={ () => { anwserQuestion(2) }}> {questions[numberOfQuestion].options[2]} </button></li>
+                    { questions[numberOfQuestion].options.map( (option, i) => 
+                         <li><button onClick={ () => { anwserQuestion(i) }}> {option} </button></li>
+                    )}
+                   
                 </ul>
             </div>
 
